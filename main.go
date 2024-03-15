7b3df8373fc7c4ec1f2a0422fd32b87d71257e62
Sai Kumar N <sai98nsk@gmail.com>
2024-03-15 12:59:34 -0400

    feat: added support to read file name and date from CLI

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"log"
+)
 
+func main() {
+	fileName := flag.String("f", "", "The filename to read")
+	date := flag.String("d", "", "The date to be parsed")
+	flag.Parse()
+	// Check if a filename was provided with the -f flag
+	if *fileName == "" {
+		log.Fatal("Please provide a filename using the -f flag.")
+		return
+	}
+	if *date == "" {
+		log.Fatal("Please provide the date -d flag")
+		return
+	}
+	fmt.Println("date", *date)
 }
