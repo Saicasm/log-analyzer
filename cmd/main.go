d8406e9e8b6305ffc0dfcf18aac17a4b30ef3249
Sai Kumar N <sai98nsk@gmail.com>
2024-03-18 19:54:32 -0400

    fix:Init log to print timestamp

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/cli/internal/logger"
 	"github.com/joho/godotenv"
 )
 
 func init() {
 	godotenv.Load()
+	logger.InitializeLogger()
 }
 func main() {
+
 	// TODO:commenting the code as only CLI mode is needed now
 	//mode := os.Getenv(constants.LOG_MODE)
 	//if mode == constants.CLI {
